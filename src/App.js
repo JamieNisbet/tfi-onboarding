5d4bad1eb390b8fac088fbdb40e82332054e6367
Jamie.Nisbet <jamie.nisbet@apoyar.eu>
2023-04-25 22:39:25 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "./App.css";
-import Header from "./templates/Header";
 import Form from "./pages/Form";
 
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <>
     <BrowserRouter>
-        <Header />
     <Routes>
       <Route path="/" element={<Form />} />
     </Routes>
